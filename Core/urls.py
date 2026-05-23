915a7c3a083a5fb598c2ba12e40debc5c8fc2b90
IcePack <mhmd.zayeri@gmail.com>
2026-05-24 02:14:46 +0330

    choose better url paths for the proj

--- Core/urls.py
+++ Core/urls.py
@@ -6,10 +6,10 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('', include('Documents.urls')),
     path('admin/', admin.site.urls),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='api_schema'), name='api_docs'),
-    path('api/schema/', SpectacularAPIView.as_view(), name='api_schema'),
-    path('api/auth/', include('Accounts.urls')),
-    path('api/jobs/', include('job.urls')),
-    path("api/quiz/", include("quiz.urls")),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='api_schema'), name='api_docs'),
+    path('schema/', SpectacularAPIView.as_view(), name='api_schema'),
+    path('auth/', include('Accounts.urls')),
+    path('jobs/', include('job.urls')),
+    path("quiz/", include("quiz.urls")),
 ]
 
